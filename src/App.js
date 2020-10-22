c87f05b7d3e7bf89d17e5a4ee0189d7603bac2e7
trishaprile <trishaprilee@gmail.com>
2020-10-22 01:11:28 -0700

    Add questions and petrs

--- src/App.js
+++ src/App.js
@@ -4,56 +4,103 @@ import ParticlesBg from "particles-bg";
 
 function App() {
 
+  var beachBum, dontTalk, eastr, grad, jolly, lucky, marchMadness, newYears, romantic, spooky, thankful, thanos;
+  beachBum = dontTalk = eastr = grad = jolly = lucky = marchMadness = newYears = romantic = spooky = thankful = thanos = 0;
+
   const questions = [
 		{
-			questionText: 'What do you like to do in your free time?',
+			questionText: 'Where do you like to study?',
 			answerOptions: [
-				{ answerText: 'New York'},
-				{ answerText: 'London'},
-				{ answerText: 'Paris'},
-				{ answerText: 'Dublin'},
+				{ answerText: 'At a coffee/boba shop', petr: 'jolly'},
+				{ answerText: "I don't", petr: 'beachBum'},
+				{ answerText: 'Anywhere with friends', petr: 'dontTalk'},
+				{ answerText: 'In the library', petr: 'grad'},
 			],
 		},
-		{
-			questionText: 'Pick a color',
+    {
+			questionText: 'How do you get around?',
 			answerOptions: [
-				{ answerText: 'Jeff Bezos'},
-				{ answerText: 'Elon Musk'},
-				{ answerText: 'Bill Gates'},
-				{ answerText: 'Tony Stark'},
+				{ answerText: 'Walking', petr: 'thanos'},
+				{ answerText: 'Riding my scooter/bike', petr: 'grad'},
+				{ answerText: 'Driving', petr: 'jolly'},
+        { answerText: 'Taking an Anteater Express Bus', petr: 'newYears'},
 			],
-		},
-		{
-			questionText: 'Choose a career',
+    },
+    {
+			questionText: 'What do you buy on Ring Road?',
 			answerOptions: [
-				{ answerText: 'Apple'},
-				{ answerText: 'Intel'},
-				{ answerText: 'Amazon'},
-				{ answerText: 'Microsoft'},
+				{ answerText: 'Boba', petr: 'romantic'},
+				{ answerText: 'Spam musubi', petr: 'dontTalk'},
+				{ answerText: 'Costco pizza and churros', petr: 'thanos'},
+				{ answerText: "Nothing, I'm broke", petr: 'newYears'},
 			],
-		},
-		{
-			questionText: 'Which word describes you the most?',
+    },
+    {
+			questionText: 'What do you do during class?',
 			answerOptions: [
-				{ answerText: '1'},
-				{ answerText: '4'},
-				{ answerText: '6'},
-				{ answerText: '7'},
+				{ answerText: 'Pay attention and take notes', petr: 'grad'},
+				{ answerText: 'Scroll through Confessions at UCI and Zotmemes', petr: 'thanos'},
+				{ answerText: 'Sleep', petr: 'beachBum'},
+        { answerText: 'Go on Tinder', petr: 'romantic'},
 			],
-		},
+    },
+    {
+			questionText: 'What do you do after class?',
+			answerOptions: [
+				{ answerText: 'Work out at the ARC', petr: 'marchMadness'},
+				{ answerText: 'Study at Langson', petr: 'grad'},
+				{ answerText: 'Grab food at UTC', petr: 'jolly'},
+				{ answerText: 'Take a nap', petr: 'beachBum'},
+			],
+    },
+    {
+			questionText: 'What are you doing on a Friday night?',
+			answerOptions: [
+				{ answerText: 'Eating dinner at Diamond Jamboree', petr: 'jolly'},
+				{ answerText: 'Hanging out at Turtle Rock', petr: 'thanos'},
+				{ answerText: 'Attending a club event', petr: 'jolly'},
+        { answerText: 'Staying in', petr: 'beachBum'},
+			],
+    },
+    {
+			questionText: 'How do you spend your Saturdays?',
+			answerOptions: [
+				{ answerText: 'Shopping at Irvine Spectrum', petr: 'jolly'},
+				{ answerText: 'Playing sports or hiking', petr: 'marchMadness'},
+				{ answerText: 'Studying and homework', petr: 'grad'},
+        { answerText: 'Playing video games', petr: 'beachBum'},
+			],
+    },
+    {
+			questionText: 'Someone with a clipboard approaches you at Student Center. What do you do?',
+			answerOptions: [
+				{ answerText: 'Listen to what they have to say', petr: 'jolly'},
+				{ answerText: "Pretend you don't see them", petr: 'romantic'},
+				{ answerText: 'Tell them you have to go to class', petr: 'grad'},
+        { answerText: 'Run away', petr: 'thanos'},
+      ],
+    }
   ];
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showResult, setShowResult] = useState(false);
+  const [petr, setPetr] = useState(0);
+  const [petrImg, setPetrImg] = useState(require('./img/petr.jpg'));
 
-  const handleAnswerOptionClick = (isCorrect) => {
+  const handleAnswerOptionClick = (petr) => {
+    petr += 1;
 		const nextQuestion = currentQuestion + 1;
 		if (nextQuestion < questions.length) {
 			setCurrentQuestion(nextQuestion);
 		} else {
-			setShowResult(true);
+      setShowResult(true);
+      setPetrImg(require('./img/beach_bum_petr.jpg'))
 		}
-	};
+  };
+  
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
   return (
     <div className="App">
@@ -61,14 +108,14 @@ function App() {
         <ParticlesBg type="circle" bg={true}/>
       </div>
       <header className="App-header">
-        Which petr are you?
+        <h1 class="animate_animated animate_bounce">Which petr are you?</h1>
       </header>
-      <img src={require('./img/petr.jpg')} alt="petr"/>
+      <img src={petrImg} alt="Petr Sticker" class="animate_animated animate_bounce"/>
       <div className="Quiz">
         {showResult ? (
           <div className='score-section'>
-            You got:
-            beach bum petr
+            <h1>beach bum petr</h1>
+            <button onClick={refreshPage}>Take Again</button>
           </div>
         ) : (
           <>
@@ -91,32 +138,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
