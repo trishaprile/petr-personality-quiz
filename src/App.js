11616aa049f5e613e97c467ffbf25f935a4337a1
trishaprile <trishaprilee@gmail.com>
2020-10-21 17:34:11 -0700

    Add quiz section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ParticlesBg from "particles-bg";
 
 function App() {
+
+  const questions = [
+		{
+			questionText: 'What do you like to do in your free time?',
+			answerOptions: [
+				{ answerText: 'New York'},
+				{ answerText: 'London'},
+				{ answerText: 'Paris'},
+				{ answerText: 'Dublin'},
+			],
+		},
+		{
+			questionText: 'Pick a color',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos'},
+				{ answerText: 'Elon Musk'},
+				{ answerText: 'Bill Gates'},
+				{ answerText: 'Tony Stark'},
+			],
+		},
+		{
+			questionText: 'Choose a career',
+			answerOptions: [
+				{ answerText: 'Apple'},
+				{ answerText: 'Intel'},
+				{ answerText: 'Amazon'},
+				{ answerText: 'Microsoft'},
+			],
+		},
+		{
+			questionText: 'Which word describes you the most?',
+			answerOptions: [
+				{ answerText: '1'},
+				{ answerText: '4'},
+				{ answerText: '6'},
+				{ answerText: '7'},
+			],
+		},
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowResult(true);
+		}
+	};
+
   return (
     <div className="App">
       <header className="App-header">
-        Which Petr Are You?
+        Which petr are you?
       </header>
       <div>
         <ParticlesBg type="circle" bg={true}/>
       </div>
-
+      <div className="Quiz">
+        {showResult ? (
+          <div className='score-section'>
+            You got:
+            beach bum petr
+          </div>
+        ) : (
+          <>
+            <div className='question-section'>
+              <div className='question-count'>
+                <span>Question {currentQuestion + 1}</span>/{questions.length}
+              </div>
+              <div className='question-text'>{questions[currentQuestion].questionText}</div>
+            </div>
+            <div className='answer-section'>
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
