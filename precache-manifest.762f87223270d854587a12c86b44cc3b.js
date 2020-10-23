92393429d6f760f60354d0d7629c552c78b5a011
trishaprile <trishaprilee@gmail.com>
2020-10-23 16:22:33 -0700

    Updates

---
+++ precache-manifest.762f87223270d854587a12c86b44cc3b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01806c6cfe34394d936f81e8b8658d26",
+    "url": "/petr-personality-quiz/index.html"
+  },
+  {
+    "revision": "d7e479bc75593d056aad",
+    "url": "/petr-personality-quiz/static/css/main.87973f39.chunk.css"
+  },
+  {
+    "revision": "a6e767127cef48b563b9",
+    "url": "/petr-personality-quiz/static/js/2.e83a0d0b.chunk.js"
+  },
+  {
+    "revision": "7e29404ea47b60d5472c72345aa9a646",
+    "url": "/petr-personality-quiz/static/js/2.e83a0d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e479bc75593d056aad",
+    "url": "/petr-personality-quiz/static/js/main.5a93c808.chunk.js"
+  },
+  {
+    "revision": "a1dcced1fc6ce42c5a0f",
+    "url": "/petr-personality-quiz/static/js/runtime-main.89169752.js"
+  },
+  {
+    "revision": "e202c2da3ada887c2f44c5614acf10bc",
+    "url": "/petr-personality-quiz/static/media/beach_bum_petr.e202c2da.jpg"
+  },
+  {
+    "revision": "a9d5f50661b7ce1a1c382aea514d776f",
+    "url": "/petr-personality-quiz/static/media/dont_talk_petr.a9d5f506.jpg"
+  },
+  {
+    "revision": "fd9c85cfcc7d63268b7a9e8d147bbcfb",
+    "url": "/petr-personality-quiz/static/media/grad_petr.fd9c85cf.jpg"
+  },
+  {
+    "revision": "56f9443e68d3d653d2b84f0330f1909f",
+    "url": "/petr-personality-quiz/static/media/jolly_petr.56f9443e.jpg"
+  },
+  {
+    "revision": "6d5599beddb35f5535fbca34ff5cb6f1",
+    "url": "/petr-personality-quiz/static/media/march_madness_petr.6d5599be.jpg"
+  },
+  {
+    "revision": "d78565aee89b79596c5661f318cae3a9",
+    "url": "/petr-personality-quiz/static/media/new_years_petr.d78565ae.jpg"
+  },
+  {
+    "revision": "8913c530334bf44a72bb49655e885d55",
+    "url": "/petr-personality-quiz/static/media/petr.8913c530.jpg"
+  },
+  {
+    "revision": "bc5ac6af8d0d81c112e4a667484d0709",
+    "url": "/petr-personality-quiz/static/media/petr_mystery.bc5ac6af.png"
+  },
+  {
+    "revision": "f2f702a37a4c6df0ab53d05dbfa3e191",
+    "url": "/petr-personality-quiz/static/media/romantic_petr.f2f702a3.jpg"
+  },
+  {
+    "revision": "2aceacdfcf0276a5edd08cf88b3fa72b",
+    "url": "/petr-personality-quiz/static/media/thanos_petr.2aceacdf.jpg"
+  }
+]);
