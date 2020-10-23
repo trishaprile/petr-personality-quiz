1f6a871531ed4b76fd4274a3a440b7cadcc4b590
trishaprile <trishaprilee@gmail.com>
2020-10-23 15:30:28 -0700

    Add descriptions

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const questions = [
         questionText: "Where do you like to study?",
         answerOptions: [
             { answerText: "At a coffee/boba shop", petr: "jolly"},
-            { answerText: "Anywhere with friends", petr: "dontTalk"},
+            { answerText: "Anywhere with friends", petr: "newYears"},
             { answerText: "In the library", petr: "grad"},
             { answerText: "I don't", petr: "beachBum"}
         ]
@@ -11,19 +11,19 @@ export const questions = [
     {
         questionText: "How do you get around?",
         answerOptions: [
-            { answerText: "Walking", petr: "thanos"},
-            { answerText: "Riding my scooter/bike", petr: "grad"},
+            { answerText: "Walking", petr: "romantic"},
+            { answerText: "Riding my scooter/bike", petr: "marchMadness"},
             { answerText: "Driving", petr: "jolly"},
-            { answerText: "Taking an Anteater Express Bus", petr: "newYears"}
+            { answerText: "Taking an Anteater Express Bus", petr: "dontTalk"}
         ]
     },
     {
         questionText: "What do you buy on Ring Road?",
         answerOptions: [
-            { answerText: "Boba", petr: "newYears"},
-            { answerText: "Spam musubi", petr: "dontTalk"},
-            { answerText: "Something new", petr: "thanos"},
-            { answerText: "Nothing, I'm broke", petr: "romantic"}
+            { answerText: "Boba", petr: "dontTalk"},
+            { answerText: "Spam musubi", petr: "marchMadness"},
+            { answerText: "Something new", petr: "newYears"},
+            { answerText: "Nothing, I'm broke", petr: "thanos"}
         ]
     },
     {
@@ -40,23 +40,23 @@ export const questions = [
         answerOptions: [
             { answerText: "Work out at the ARC", petr: "marchMadness"},
             { answerText: "Study at Langson", petr: "grad"},
-            { answerText: "Grab food at UTC", petr: "jolly"},
+            { answerText: "Grab food at UTC", petr: "dontTalk"},
             { answerText: "Take a nap", petr: "beachBum"}
         ]
     },
     {
         questionText: "What are you doing on a Friday night?",
         answerOptions: [
-            { answerText: "Eating dinner at Diamond Jamboree", petr: "jolly"},
-            { answerText: "Hanging out at Turtle Rock", petr: "thanos"},
+            { answerText: "Eating dinner at Diamond Jamboree", petr: "newYears"},
+            { answerText: "Hanging out at Turtle Rock", petr: "romantic"},
             { answerText: "Attending a club event", petr: "jolly"},
-            { answerText: "Staying in", petr: "beachBum"}
+            { answerText: "Staying in", petr: "thanos"}
         ]
     },
     {
         questionText: "How do you spend your Saturdays?",
         answerOptions: [
-            { answerText: "Shopping at Irvine Spectrum", petr: "jolly"},
+            { answerText: "Shopping at Irvine Spectrum", petr: "newYears"},
             { answerText: "Playing sports or hiking", petr: "marchMadness"},
             { answerText: "Studying and homework", petr: "grad"},
             { answerText: "Playing video games", petr: "beachBum"}
@@ -66,8 +66,8 @@ export const questions = [
         questionText: "Someone with a clipboard approaches you at Student Center. What do you do?",
         answerOptions: [
             { answerText: "Listen to what they have to say", petr: "jolly"},
-            { answerText: "Pretend you don't see them", petr: "romantic"},
-            { answerText: "Tell them you have to go to class", petr: "grad"},
+            { answerText: "Pretend you don't see them", petr: "dontTalk"},
+            { answerText: "Tell them you have to go to class", petr: "romantic"},
             { answerText: "Run away", petr: "thanos"}
         ]
     }
@@ -78,14 +78,14 @@ export const petrs = [
         id: "beachBum",
         name: "beach bum petr",
         img: "./img/beach_bum_petr.jpg",
-        description: "You're laid back and down to earth. You enjoy relaxing . You're easy to talk to ",
+        description: "You're laid back and down to earth. You enjoy relaxing and you're easy to talk to. You often procrastinate and sleep too much, but you always get your work done in time. You never pay attention in class and you feel the most comfortable at home.",
         points: 0
     },
     { 
         id: "dontTalk",
         name: "don't talk to me or my petrs",
         img: "./img/dont_talk_petr.jpg",
-        description: "You care a lot about the people closest to you, but you can get overprotective sometimes. You're the parent of the group and you're always taking care of the people around you. People look up to you and come to you for advice. You spend most of your time at UTC or other people's apartments and.",
+        description: "You care a lot about the people closest to you, but you can get overprotective sometimes. You're the parent of the group and you're always watching over others around you. People look up to you and come to you for advice. People also enjoy your company and you make friends easily.",
         points: 0
     },
     { 
@@ -99,35 +99,35 @@ export const petrs = [
         id: "jolly",
         name: "jolly petr",
         img: "./img/jolly_petr.jpg",
-        description: "",
+        description: "You're high-spirited and thoughtful. You like making other people happy and you find joy in the little things. You're a very generous person and you often put others before yourself. You also light up every room you walk into.",
         points: 0
     },
     { 
         id: "marchMadness",
         name: "march madness petr",
         img: "./img/march_madness_petr.jpg",
-        description: "You're active . You spend most of your time outside or at the ARC. You work well in groups and you're a natural leader. You can get competitive sometimes, but you always have a positive attitude.",
+        description: "You're active and energetic. You work well in groups and you're a natural leader. You can get competitive sometimes, but you always have a positive attitude. You're good at almost everything you do and you easily overcome challenges.",
         points: 0
     },
     { 
         id: "newYears",
         name: "new year new petr",
         img: "./img/new_years_petr.jpg",
-        description: "",
+        description: "You're an outgoing person who enjoys trying new things. You're always around other people and tend to be out and about most of the time. Your personality and style are constantly changing and you have really good taste. People enjoy your presence, but you can get wild sometimes.",
         points: 0
     },
     { 
         id: "romantic",
         name: "romantic petr",
         img: "./img/romantic_petr.jpg",
-        description: "",
+        description: "You're an understanding and loving person. You put a lot of effort into your relationships and always want the best for others. You like spending quality time with a few close people in your life and you tend to be very optimistic most of the time.",
         points: 0
     },
     {
         id: "thanos", 
         name: "thanos petr",
         img: "./img/thanos_petr.jpg",
-        description: "You like everything to have balance in your life. You tend to take risks for the greater good.",
+        description: "You like everything to have balance in your life. You tend to take a lot of risks and you want to do things for the greater good. You don't care what people think about you and you have a strong will. You often do whatever it takes to get to where you want to be.",
         points: 0
     }
 ];
